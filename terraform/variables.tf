f06a5ef7f5d743a7850e5b901c1908d5181a96db
Leo Bach <64045074+MachineSaver@users.noreply.github.com>
2025-11-14 02:45:40 -0600

    Kindness Update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,21 +1,61 @@
-variable "linode_token"      { type = string, sensitive = true }
-variable "label"             { type = string, default = "ms-edge-01" }
-variable "region"            { type = string, default = "us-east" }
-variable "type"              { type = string, default = "g6-nanode-1" }
-variable "image"             { type = string, default = "linode/ubuntu24.04" }
-variable "ssh_pub_key"       { type = string, sensitive = true }
-variable "root_pass"         { type = string, sensitive = true }
+variable "linode_token" {
+  type      = string
+  sensitive = true
+}
 
-variable "domain"            { type = string }            # e.g., "machinesaver.com"
-variable "repo_url"          { type = string, default = "https://github.com/Machine-Saver-Inc/AirVibe_Waveform_Edge" }
+variable "label" {
+  type    = string
+  default = "ms-edge-01"
+}
 
-# For acme.sh DNS-01 on the VPS
-variable "cloudflare_api_token" { type = string, sensitive = true }
-variable "acme_email"           { type = string }
+variable "region" {
+  type    = string
+  default = "us-east"
+}
 
-# For creating the DNS A record via Terraform
-variable "cloudflare_zone_id" { type = string }           # zone id for domain
+variable "type" {
+  type    = string
+  default = "g6-nanode-1"
+}
 
-# Public Issuing CA cert (client-auth) injected into mosquitto
-variable "issuing_ca_cert_pem" { type = string }
+variable "image" {
+  type    = string
+  default = "linode/ubuntu24.04"
+}
 
+variable "ssh_pub_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "root_pass" {
+  type      = string
+  sensitive = true
+}
+
+variable "domain" {
+  type = string
+}
+
+variable "repo_url" {
+  type    = string
+  default = "https://github.com/Machine-Saver-Inc/AirVibe_Edge"
+}
+
+variable "cloudflare_api_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "acme_email" {
+  type = string
+}
+
+variable "cloudflare_zone_id" {
+  type = string
+}
+
+# Public Issuing CA cert (PEM) injected into mosquitto
+variable "issuing_ca_cert_pem" {
+  type = string
+}
